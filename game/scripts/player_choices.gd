1cbb957f3f619cc56c3953ac67aeef4e672f04f9
unknown <erikaporrasgutierrez@gmail.com>
2024-01-25 16:16:57 -1000

    interrogate

---
+++ game/scripts/player_choices.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+
+func _on_interrogate_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/player_choices_interrogate.tscn")
---
+++ game/scripts/player_choices_interrogate.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+
+func _on_interrogate_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/player_choices.tscn")
